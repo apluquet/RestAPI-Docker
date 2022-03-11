770fef7bba2080f95778d9acd4281dbc82b2b624
Adrien ANTON LUDWIG <adrien.antonludwig@gmail.com>
2022-03-11 17:07:41 +0100

    [ADD] stage2: gunicorn

---
+++ solutions/stage2/sources/OCR_route.py
@@ -0,0 +1,33 @@
+from io import BytesIO
+from flask import Flask, request, jsonify
+from PIL import Image, UnidentifiedImageError
+
+app = Flask(__name__)
+
+
+@app.route("/imgshape", methods=["POST"])
+def imgshape():
+    image_data = request.data
+    # We expect image/jpg Content type
+    if image_data is None:
+        return jsonify({"error": "Request contains no image data."}), 500
+
+    width = height = depth = None
+
+    # write image to disk (unless you find a better way)
+    try:
+        # use opencv (headless) or some better choice to read the image and get its shape
+        image = Image.open(BytesIO(image_data))
+        width, height = image.size
+        depth = len(image.getbands())
+    except UnidentifiedImageError:
+        return jsonify({"error": "Cannot open image."}), 422
+    except Exception as err:
+        return jsonify({"error": f"Unknown error: {err}"}), 500
+
+    return jsonify({"content": {"width": width, "height": height, "depth": depth}}), 200
+
+
+if __name__ == "__main__":
+    # Simply running this file will start a Flask server in development mode.
+    app.run(host="0.0.0.0", debug=True)
