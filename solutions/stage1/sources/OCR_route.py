ec3b8e15760a7218ce456903aaa71f6d6b80b2c7
Adrien ANTON LUDWIG <adrien.antonludwig@gmail.com>
2022-03-11 16:35:26 +0100

    [ADD] Dockerfile, requirements and docer-compose

--- solutions/stage1/sources/OCR_route.py
+++ solutions/stage1/sources/OCR_route.py
@@ -1,12 +1,6 @@
 from io import BytesIO
-import tempfile
-from pathlib import Path
-
 from flask import Flask, request, jsonify
-from werkzeug.utils import secure_filename
-
 from PIL import Image, UnidentifiedImageError
-
 
 app = Flask(__name__)
 
@@ -20,17 +14,16 @@ def imgshape():
 
     width = height = depth = None
 
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        # write image to disk (unless you find a better way)
-        try:
-            # use opencv (headless) or some better choice to read the image and get its shape
-            image = Image.open(BytesIO(image_data))
-            width, height = image.size
-            depth = len(image.getbands())
-        except UnidentifiedImageError:
-            return jsonify({"error": "Cannot open image."})
-        except Exception as err:
-            return jsonify({"error": f"Unknown error: {err}"}), 500
+    # write image to disk (unless you find a better way)
+    try:
+        # use opencv (headless) or some better choice to read the image and get its shape
+        image = Image.open(BytesIO(image_data))
+        width, height = image.size
+        depth = len(image.getbands())
+    except UnidentifiedImageError:
+        return jsonify({"error": "Cannot open image."}), 422
+    except Exception as err:
+        return jsonify({"error": f"Unknown error: {err}"}), 500
 
     return jsonify({"content": {"width": width, "height": height, "depth": depth}}), 200
 
